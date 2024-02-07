de615e74de92df76b0b33cf80ea2f36c8ce58e5d
exeldrian <65379138+exeldrian@users.noreply.github.com>
2024-02-07 10:11:02 -0500

    Add files via upload

--- convert_all.ps1
+++ convert_all.ps1
@@ -13,41 +13,64 @@ NOTE: there are tens of thousands of files to convert and move. Dis gon' take ti
 
 Clear-Host
 
-# Build list of all .cga files in folder, recursively
+# Ask where the files are at
+$location = Read-Host -Prompt "Where is your unpacked 'objects' folder located? I.E. C:\SC_Unpacked\"
+
+# People don't pay attention to detail, add a backslash if missing
+if ($location -match '\\$') {
+}
+else {
+  $location = $location + "\"
+}
+
+# This is what we expect to find. We need this file to be able to do our thing
+$converterExe = $location + "cgf-converter.exe"
+Write-Output "Checking in:" $location "`r"
+if(Test-Path $converterExe -PathType Leaf)
+{
+    Write-Output "CGF Converter found. Continuing..."
+}
+else {
+    Write-Output "CGF Converter not found! Make sure it is in the directory you specified! Aborting...`r`n"
+    exit
+}
+
+# Build lists of all .cga and .cgf files in folder, recursively
 Write-Output "Building list of files, please wait...`r`n"
-$files = Get-ChildItem -path "E:\SC_Ships\3_22\" -Recurse | Where-Object {$_.extension -eq ".cga"}
-$files2 = Get-ChildItem -path "E:\SC_Ships\3_22\" -Recurse | Where-Object {$_.extension -eq ".cgf"}
+$files = Get-ChildItem -path $location -Recurse | Where-Object {$_.extension -eq ".cga"}
+$files2 = Get-ChildItem -path $location -Recurse | Where-Object {$_.extension -eq ".cgf"}
 
 # What file type do you want?
 $format = Read-Host -Prompt "What format do you want the files converted to? [-dae, -obj]"
 
-# For each file in the list, check if destination directory exists (make if not), convert, and move converted file to destination directory
+# For each .cga file, capture file pathing to build run parameters for converter, and convert
 foreach ($file in $files) {
 $params = $file.FullName + " " + $format + " -objectdir " + $file.DirectoryName
 $name = $file.Basename
     if ($format -eq "-obj") {
-		Start-Process -FilePath E:\SC_Ships\cgf-converter.exe $params -WindowStyle Hidden
+		Start-Process -FilePath $converterExe $params -WindowStyle Hidden
     Start-Sleep -Milliseconds 300
-		Write-Output $file
+		Write-Output $file.FullName
     }
     else {
-		Start-Process -FilePath E:\SC_Ships\cgf-converter.exe $params -WindowStyle Hidden
+		Start-Process -FilePath $converterExe $params -WindowStyle Hidden
     Start-Sleep -Milliseconds 300
-		Write-Output $file
+		Write-Output $file.FullName
     }
 }
 
+# For each .cgf file, capture file pathing to build run parameters for converter, and convert
 foreach ($file2 in $files2) {
 $params = "$file2 $format -objectdir $file2.DirectoryName"
 $name = $file2.Basename
     if ($format -eq "-obj") {
 		Start-Process -FilePath E:\SC_Ships\cgf-converter.exe $params -WindowStyle Hidden
     Start-Sleep -Milliseconds 300
-		Write-Output $file2
+		Write-Output $file2.FullName
     }
     else {
 		Start-Process -FilePath E:\SC_Ships\cgf-converter.exe $params -WindowStyle Hidden
     Start-Sleep -Milliseconds 300
-		Write-Output $file2
+		Write-Output $file2.FullName
     }
 }
